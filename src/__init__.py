74475da06ba0182f3cfbf7d02a54af4284ecb22c
derp@thinkpad2 <becker.nick.a@gmail.com>
2024-04-28 15:34:02 -0500

    ignore

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+"""hal3662 - Web services
+
+Provides
+--------
+Deployable web services with an eco-conscious billing system.
+
+Documentation
+-------------
+Documentation is provided primarily by python docstrings. The documentatation is
+hosted on GitHub Pages at the following URL: 
+
+Contributing
+------------
+See: https://github.com/HAL-Software-LLC/HAL3662
+"""
